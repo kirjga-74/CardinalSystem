457753955962703713b803fbac4c41d111164f81
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-01 09:56:20 +0500

    Update Config.py

--- Sibyl_System/Config.py
+++ Sibyl_System/Config.py
@@ -1,49 +1,12 @@
-#######START - IMPORTANT NOTICE TO READ###########
-#                                                #
-# This is an sample configuration file for       #
-# people who self-host the userbot without using #
-# environment variables and setting ENV to       #
-# 'ANYTHING'.                                    #
-#                                                #
-# ============================================== #
-#                                                #
-# Duplicate this file into 'config.py', read the #
-# comments before editing, save. Go to the last  #
-# line and set the IS_THIS_CONFIG_EXAMPLE        #
-# variable into "NOT ANYMORE" for the userbot to #
-# start. This is to ensure that people           #
-# understand how to configure this userbot.      #
-#                                                #
-#########END - IMPORTANT NOTICE TO READ###########
+from typing import Union, Optional
 
-# Telegram API Info
-# Replace placeholders with your own values. For sting token, use the sting session generator script.
-API_ID = 2147833
-API_HASH = "75aaffe84071e6ceb7ab4e07b4a4a266"
-STRING_SESSION = "1BVtsOLUBu2GXIsLKUGTZkSgwO0AEeBcEPWmsPCPFZyZnstFNn1ltw64jD2h4MqMQxHGh_8NeJuZx5h0fW5gMg8s-4IBlz6Vw2Ks_-ttOKn7jPzdL70MbxKAlCfFwh3r_4WNIasUkineZxi7IL4qu5mmQjVWBdJYwjH-TauiLgd13MWrAy9XqT0kqyP2uG7Cw0BGAGyKKFXOzhiLUg-raH7wwcDWxxMZKhSrRtJXJT9Ga3f0A9yx2OCIhAdHZUk9t2WbgWFnJfVBpqKUullUCBy18VVuxes60an-0nFMr9jD7fZ4buWUQTKqabYeJO6xcgkSVFvtVMXLe45oR53zqGrC1d8a7x-c="
-BOT_TOKEN = "1615719817:AAHqQZWGvFBXVW1lTnnkv4216aYs1wNuafk"
+API_ID: int = 3296233
+API_HASH: str = "b6315a1fc6d4d15d5278fea0c5a99c04"
+STRING_SESSION: str = "1BVtsOHcBu4oSCS5_Vfbnx8hR-eKBagqNuKIvGxVZCI4_f9KWQoFMOX47J7ZdkFgriwPQ8woPPCt83yPOxexGQdNNFu5bvl4OWTbPGl6MQgu739BD5MSGUs0Fc_LBwbWdhB4r7t7DhTyrLfqoU1hgBekEhF1ywqWs6KCwjiV5BliuHDp0UCMgbLi6s7PgNAM1w87WrNrfehhuXpImbIeD4ELhEXFqCJ7ix7KM7HUOX87ZXRVMGUatPVx3oCcYnxZPZnEXjm33TNKBYsHXvgwe2fQ_zmLKt6-qFAn0fLOUlA68T4lQksd7XZSyvN3DC5KXJCQy8-LhNGZE9NDIQfyGXK2JNZ9TNG8="
+Sibyl_logs: Union[str, int] = -569492198
+Sibyl_approved_logs: Union[str, int] = --562922694
+GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+BOT_TOKEN: str = "1729271071:AAFECOUzUuQU0jo8PFDUpBwHeGYk7JcJngM"
+MONGO_DB_URL = "mongodb+srv://tg-god-usr:xPpLPNSgvBB2OWvk@cluster0.em0bd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-# The MongoDB URL
-# Use the v3.4.x+ URL format. For 3.6.x+, make sure 'dnspython' is installed.
-MONGO_DB_URL = "mongodb+srv://sybil_use:RPMvhLqJmKbCGq1x@cluster0.amty7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-# These numbers below are real userids.
-# Replace it with your own userids. Or, remove everything and manually paste userids in JSON format.
-CARDINAL = [1418166549]
-MANAGERS = [1324456]
-DEVELOPERS = [1634756943]
-
-# These intergers below are real chatids.
-# Replace it with your own chatids or the userbot will not start.
-Sibyl_logs = -1001274776877
-Sibyl_approved_logs = -1001274776877
-GBAN_MSG_LOGS = -1001293004442
-
-########################################
-# IMPORTANT!                           #
-# Set the variable below to            #
-# "NOT ANYMORE" in order to run this   #
-# userbot without using/going          #
-# to environment variables/env mode.   #
-########################################
-IS_THIS_CONFIG_EXAMPLE = "READ THE COMMENTS ON L1-L17 FIRST!"
+# Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
