abddebfa140798cfea82bac2d68665b3bb7bb4ea
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-01 10:06:18 +0500

    Update Config.py

--- Sibyl_System/Config.py
+++ Sibyl_System/Config.py
@@ -1,7 +1,7 @@
 from typing import Union, Optional
 
-API_ID: int = 3296233
-API_HASH: str = "b6315a1fc6d4d15d5278fea0c5a99c04"
+API_ID: int = 2070907
+API_HASH: str = "e1d077ec3bdf540193afdb4d846bcfb2"
 STRING_SESSION: str = "1BJWap1sBu1IDhTkbUvudZNZSirObKkNYmKQmUjAlBt3CYxHBh7hjIWSslg-aXrqL05feoZlUoV0yna4Mv3Spv7aBMYjmVDE9Wj2iapS7Vavc1ORKrqUaECeeFlwzgXEm2wIDJSW8VQCz-gxtlUMrLYOqMaSzXKVAlv4cQBw4rfRp_Kj4RnT3f6eRF-vcWZ8-2cmJj6p9UdEA1bS3kpsIMzgrykyVaobl9fNyrpB7KJ80aNVtCQ5e6ColZ7MFkMhRsyoQAw44hReKF4g8hBgdCKE81ENliw2xBIp79BEEFcGbIzHu3k9meTaoV-hVqM-nu5HoFF5xsu_lKW8czmsriVg8qTYRTxo="
 Sibyl_logs: Union[str, int] = -569492198
 Sibyl_approved_logs: Union[str, int] = --562922694
